678c691a181a18998e3c5cb3d32ca96f31ad6229
Gabriel Leblond <120410801+gabriel-leblond@users.noreply.github.com>
2023-08-17 15:51:59 +0200

    Modele OK

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,14 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: {scope: :list_id}
 end
+
+
+# Un signet doit être lié à un film et une liste,
+# et les associations [film, liste] doivent être uniques.
+
+
+# list cannot be blank
+# is unique for a given movie/list couple (FAILED - 1)
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,13 @@
 class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  # has_many :movies, dependent: :destroy
+  has_many :movies, through: :bookmarks
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  # validates [:list_id], uniqueness: true
 end
+
+
+# Quand tu supprimes une liste, tu dois supprimer tous les signets associés (mais pas les films, car ils peuvent être indiqués en référence dans d’autres listes)
